7665ac74932485741740b5f1e30d046c9e0d9158
Tomofumi Yagi <yagitmknada@gmail.com>
2026-08-11 10:10:10 +0900

    chore: upgrade runtime to Ruby 4 and Node 24

--- shirasagi_image/run.sh
+++ shirasagi_image/run.sh
@@ -7,11 +7,6 @@ bundle config set --local without 'development test'
 bundle config set --local path 'vendor/bundle'
 bundle install
 
-# なぜか、rakeのバージョンが古い。。とりあえずgemで最新入れてもう一度bundle installしたら大丈夫
-# ここでダメな理由はいつか調べる
-gem install rake
-bundle install
-
 # Launch
 rm -f /var/www/shirasagi/tmp/pids/unicorn.pid
 bundle exec rake unicorn:start
